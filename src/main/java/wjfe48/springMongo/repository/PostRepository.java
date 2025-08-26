686c550c9d1778407d2afcea0757d0df4ef7f75d
Waldemar <maza.wjfe@gmail.com>
2025-08-26 10:34:23 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/wjfe48/springMongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package wjfe48.springMongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import wjfe48.springMongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}

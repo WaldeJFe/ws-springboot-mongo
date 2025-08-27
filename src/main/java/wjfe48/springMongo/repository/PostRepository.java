c95f93ede3cb9b07219cce9c6b66487d42e2991a
Waldemar <maza.wjfe@gmail.com>
2025-08-27 13:09:08 -0300

    Consulta simples com query methods

--- src/main/java/wjfe48/springMongo/repository/PostRepository.java
+++ src/main/java/wjfe48/springMongo/repository/PostRepository.java
@@ -1,10 +1,13 @@
 package wjfe48.springMongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import wjfe48.springMongo.domain.Post;
+import wjfe48.springMongo.domain.Post; 
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }

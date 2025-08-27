e9e73b1ae247cf9bbd684309bafff1a1435c8f9c
Waldemar <maza.wjfe@gmail.com>
2025-08-27 09:03:13 -0300

    Obtendo um post por id

---
+++ src/main/java/wjfe48/springMongo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package wjfe48.springMongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import wjfe48.springMongo.domain.Post;
+import wjfe48.springMongo.services.PostService;
+
+@RestController
+@RequestMapping(value="/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@GetMapping("/{id}")
+ 	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/wjfe48/springMongo/services/PostService.java
@@ -0,0 +1,21 @@
+package wjfe48.springMongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import wjfe48.springMongo.domain.Post;
+import wjfe48.springMongo.repository.PostRepository;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
